b2e441d6ed551e9e7baffbd0a6a68dd90443c93a
Marcel Mozet <marcel.mozet@stud.uni-regensburg.de>
2026-06-22 20:07:26 +0200

    hand recognition for task 1

---
+++ hand_recognition/mediapipe_hands.py
@@ -0,0 +1,93 @@
+# code from google mediapipe's code sample
+# adapted to be compatible with opencv live image stream
+# https://developers.google.com/edge/mediapipe/solutions/vision/hand_landmarker/python
+
+import sys
+import cv2
+import time
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import threading
+
+
+class HandRecognizerThread:
+    def __init__(self, video_id, num_hands, model_path):
+        self.num_hands = num_hands
+
+        OPTIONS = vision.HandLandmarkerOptions(
+            base_options=python.BaseOptions(model_asset_path=model_path),
+            num_hands=self.num_hands,
+            running_mode=vision.RunningMode.VIDEO,
+            min_hand_detection_confidence=0.5,
+            min_hand_presence_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
+        self.detector = vision.HandLandmarker.create_from_options(OPTIONS)
+        self.cap = cv2.VideoCapture(video_id)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, sys.maxsize)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, sys.maxsize)
+
+        self.results = {}
+
+        self.interrupt_event = threading.Event()
+        self.lock = threading.Lock()
+
+    def interrupt(self):
+        self.interrupt_event.set()
+
+    def run(self):
+        while not self.interrupt_event.is_set():
+            # Capture a frame from the webcam
+            ret, frame = self.cap.read()
+            frame = cv2.flip(frame, 1)
+            frame_y, frame_x = frame.shape[:2]
+
+            # convert to mediapipe image
+            mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+
+            # save current time for internal interpolation
+            timestamp_ms = int(self.cap.get(cv2.CAP_PROP_POS_MSEC))
+
+            # Detect hand landmarks from the input image.
+            detection_result = self.detector.detect_for_video(
+                mp_frame, timestamp_ms)
+
+            hand_landmarks_list = detection_result.hand_landmarks
+            if len(hand_landmarks_list) > 0:
+                landmarks = hand_landmarks_list[0]
+
+                rel_index_x = landmarks[8].x
+                rel_index_y = landmarks[8].y
+
+                rel_middle_x = landmarks[12].x
+                rel_middle_y = landmarks[12].y
+
+                rel_thumb_x = landmarks[4].x
+                rel_thumb_y = landmarks[4].y
+
+                index = {"x": rel_index_x, "y": rel_index_y}
+                middle_finger = {"x": rel_middle_x, "y": rel_middle_y}
+                thumb = {"x": rel_thumb_x, "y": rel_thumb_y}
+
+                with self.lock:
+                    self.results = {
+                        "index": index,
+                        "middle_finger": middle_finger,
+                        "thumb": thumb
+                    }
+
+            else:
+                with self.lock:
+                    self.results.clear()
+
+                # print(f"middle at {landmarks[12][1]}, {landmarks[12][2]}")
+                # print(f"thumb at {landmarks[4][1]}, {landmarks[4][2]}")
+
+                # 8 = Zeigefinger
+                # 4 daumen
+                # 12 Mittelfinger
+        self.stop()
+
+    def stop(self):
+        self.cap.release()
