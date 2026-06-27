267d0dd54028b7975d97aa50f375eb30beb6919c
Marcel Mozet <marcel.mozet@stud.uni-regensburg.de>
2026-06-27 14:07:30 +0200

    add latency to fitts law

--- fitts_law.py
+++ fitts_law.py
@@ -10,6 +10,7 @@ import os
 import csv
 import random
 from pathlib import Path
+from collections import deque
 
 participantID = 0
 trials = 3
@@ -41,7 +42,12 @@ combi_amount = 0
 PADDING = 50
 
 circles = []
+mouse_circle = None
+
 batch = pyglet.graphics.Batch()
+
+motion_events = deque()
+click_events = deque()
 
 # first command line input is participant-ID
 if len(sys.argv) > 1:
@@ -169,8 +175,39 @@ createCircles()
 setupFL()
 
 
+def create_mouse_circle(x, y):
+    global mouse_circle
+
+    RADIUS = 5
+    center_offset = RADIUS//2
+    mouse_circle = shapes.Circle(
+        x - center_offset, y - center_offset, radius=RADIUS, color=(0, 255, 0), batch=batch)
+
+
+def update_mouse_circle(target_x, target_y):
+    global mouse_circle
+
+    center_offset = mouse_circle.radius // 2
+    mouse_circle.x = target_x - center_offset
+    mouse_circle.y = target_y - center_offset
+
+
 @win.event
 def on_mouse_press(x, y, button, modifiers):
+    global input
+    t = time.time()
+
+    if inputs[input] == "latency":
+        t = time.time() + 0.150 
+
+    click_events.append({
+        "t": t,
+        "x": x,
+        "y": y,
+    })
+
+
+def perform_click_event(x, y):
     global target_w, target_r, current_target, target_amount, counter, trial_count, trials, combi_count, combi_amount
     if x > circles[int(current_target)].circle.x - target_r and x < circles[int(current_target)].circle.x + target_r and y > circles[int(current_target)].circle.y - target_r and y < circles[int(current_target)].circle.y + target_r:
         if counter == target_amount-1:
@@ -194,42 +231,48 @@ def on_mouse_press(x, y, button, modifiers):
             updateFl()
 
 
-mouse_circle = None
-
-def create_mouse_circle(x, y):
-    global mouse_circle
-    
-    RADIUS = 5
-    center_offset = RADIUS//2
-    mouse_circle = shapes.Circle(
-        x - center_offset, y - center_offset, radius=RADIUS, color=(0, 255, 0), batch=batch)
-    
-
-
-def update_mouse_circle(target_x, target_y):
-    global mouse_circle
-    
-    center_offset = mouse_circle.radius // 2
-    mouse_circle.x = target_x - center_offset
-    mouse_circle.y = target_y - center_offset
-
-
-
 @win.event
 def on_mouse_enter(x, y):
     create_mouse_circle(x, y)
 
+
 @win.event
 def on_mouse_motion(x, y, dx, dy):
+    global input
+    t = time.time()
+
+    if inputs[input] == "latency":
+        t = time.time() + 0.150 
+
+    motion_events.append({
+        "t": t,
+        "x": x,
+        "y": y,
+        "dx": dx
+    })
+    
+
+
+def perform_motion_event(x, y):
     global mouse_circle
     if mouse_circle is not None:
         update_mouse_circle(x, y)
-
-
+        
+def mouse_events_polling(dt):
+    t = time.time()
+    while motion_events and t >= motion_events[0]["t"]:
+        m_event = motion_events.popleft()
+        perform_motion_event(m_event["x"], m_event["y"])
+        
+    t = time.time()
+    while click_events and t >= click_events[0]["t"]:
+        c_event = click_events.popleft()
+        perform_click_event(c_event["x"], c_event["y"])
+        
 @win.event
 def on_draw():
     win.clear()
     batch.draw()
 
-
+pyglet.clock.schedule_interval(mouse_events_polling, 0.005)
 pyglet.app.run()
