038d0a5a9cfec3f15e8b0568732142d61740a0d7
itiswhatitis1994 <Vanessa.Brandwirth@stud.uni-regensburg.de>
2026-06-30 14:11:17 +0200

    add plot creation (fitt's law)

--- plot_creation.py
+++ plot_creation.py
@@ -5,8 +5,45 @@ import os
 
 
 def add_fitts_index_of_difficulty_column(data):
-    pass
+    data = data.copy()
+    data["difficulty"] = np.log2(data["target_d"]/data["target_w"] + 1)
+    return data
 
+def add_fitts_mt_column(data):
+    data = data.copy()
+    data["mt"] = np.nan
+
+    for i in range(1, len(data)):
+        if data.iloc[i]["target_id"] != 0:
+            data.loc[i, "mt"] = (
+                data.iloc[i]["timestamp"] -
+                data.iloc[i-1]["timestamp"]
+            )
+
+    return data
+
+def add_fitts_throughput_column(data):
+    data = data.copy()
+    data["throughput"] = data["difficulty"] / data["mt"]
+    return data
+
+def transform_fitts_to_single_line_cond(data):
+    data = data.copy()
+    mean_mt = (
+        data
+        .groupby(["iteration","input", "difficulty"])["mt"]
+        .mean()
+        .reset_index()
+    )
+    return mean_mt
+
+def prep_fitts_data(data):
+    data = data.copy()
+    data = add_fitts_index_of_difficulty_column(data)
+    data = add_fitts_mt_column(data)
+    data = transform_fitts_to_single_line_cond(data)
+    data = add_fitts_throughput_column(data)
+    return data
 
 def prepate_steering_data(data):
     data = data.copy()
@@ -32,6 +69,63 @@ def read_steering_data():
 
     complete_df = pd.concat(dfs)
     return complete_df
+
+def read_fitts_data():
+    data_dir = "./data/"
+    dfs = []
+    for dirpath, dirnames, filenames in os.walk(data_dir):
+        for file in filenames:
+            if file.endswith(".csv") and file[:5] == "fitts":
+                full_path = os.path.join(dirpath, file)
+                df = pd.read_csv(full_path)
+                dfs.append(df)
+
+    complete_df = pd.concat(dfs)
+    return complete_df
+
+def create_fitts_mt_plot(data):
+    inputs = data["input"].unique()
+
+    fig, axes = plt.subplots(
+        2, 2,
+        figsize=(12, 8),
+        sharex=True,
+        sharey=True
+    )
+
+    axes = axes.flatten()
+
+    for ax, input_mode in zip(axes, inputs):
+        subset = data[data["input"] == input_mode]
+
+        subset = subset.sort_values("difficulty")
+
+        ax.scatter(
+            subset["difficulty"],
+            subset["mt"]
+        )
+
+        mean_data = (
+            subset
+            .groupby("difficulty")["mt"]
+            .mean()
+            .reset_index()
+            .sort_values("difficulty")
+        )
+
+        ax.plot(
+            mean_data["difficulty"],
+            mean_data["mt"],
+            linewidth=2,
+            label="Mean MT"
+)
+
+        ax.set_title(input_mode)
+        ax.set_xlabel("ID (bits)")
+        ax.set_ylabel("MT (s)")
+
+    plt.tight_layout()
+    plt.savefig("./plots/fitts_mt_subplots.png")
 
 
 def create_steering_time_plot(data):
@@ -73,6 +167,10 @@ def create_steering_time_plot(data):
     plt.savefig("./plots/steering_times.png")
 
 
-complete_df = read_steering_data()
-complete_df = prepate_steering_data(complete_df)
-create_steering_time_plot(complete_df)
+steering_df = read_steering_data()
+steering_df = prepate_steering_data(steering_df)
+create_steering_time_plot(steering_df)
+
+fitts_df = read_fitts_data()
+fitts_df = prep_fitts_data(fitts_df)
+create_fitts_mt_plot(fitts_df)
